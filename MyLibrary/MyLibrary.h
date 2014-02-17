e9c1ce83bc79673f074da4a0dff6ddcc37c08a63
kwakata <kwakata@sonix.asia>
2014-02-17 11:32:05 +0900

    test code

---
+++ MyLibrary/MyLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MyLibrary.h
+//  MyLibrary
+//
+//  Created by kwakata on 2014/02/17.
+//  Copyright (c) 2014年 kwakata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLibrary : NSObject
+
++ (void)test;
+
+@end
